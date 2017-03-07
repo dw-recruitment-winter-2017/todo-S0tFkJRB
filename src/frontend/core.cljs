05e6564a1a256aa34ec1dc96d90b6b848dfb718e
Anonymous Applicant <anonymous@example.com>
2017-03-07 08:18:12 -0500

    Start rendering the frontend (ever so slightly)

--- src/frontend/core.cljs
+++ src/frontend/core.cljs
@@ -4,13 +4,26 @@
 
 (enable-console-print!)
 
+(defonce app-state (r/atom {:todos {}}))
+
+(defn get-todo [id]
+  (get (:todos @app-state) id))
+
+(defn load-todos! [new-todos]
+  (reset! app-state
+    {:todos (zipmap (map (fn [todo] (:id todo)) new-todos) new-todos)}))
+
+(defn app [props]
+  (println (:todos @app-state)))
+
 (defn load-app []
-  (println "Hey there")
   (ajax/ajax-request
     {:uri "/api/todo"  ;; Would be better to use whatever `url-for` is
      :method :get
      :response-format (ajax/json-response-format {:keywords? true})
      :handler (fn [[ok res]]
-                (if ok (println (str res))))}))
+                (if ok ((load-todos! (js->clj res))
+                        (r/render [app] (js/document.getElementById "app")))
+                  (println (str res))))}))
 
 (defn ^:export run [] (load-app))
